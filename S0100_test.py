dd424e54fc4ea6061c0cb602ba863590ff51ea03
Sleepy31415 <sleepybearz19@gmail.com>
2023-03-21 09:46:16 +0100

    test

---
+++ S0100_test.py
@@ -0,0 +1,32 @@
+"""
+Exercise "Test pycharm and github":
+
+As always, read the whole exercise description carefully before you begin to solve the exercise.
+
+Copy this file into your own solution directory.
+    In the opgaver project, click on the file name in the Project Files window and press CTRL+C.
+    Switch to the solutions project, click on the main folder in the Project Files window and press CTRL+V.
+
+Edit your copy only.
+
+Run the program with shift+f10 (or click on the green "play" icon)
+
+In the program code, change the text between the quotation marks
+
+Commit the file to your own git repository (commit means: back up the current state of your files as a new version)
+    Git, Commit (or click the green check mark)
+    write a meaningful comment
+    click on "commit and push" (push means: upload this commit to github)
+    push
+
+Check in your web browser on github.com that the changed file is present in your repository on github.
+
+On the GitHub web page of your repository, click on "Settings", make sure that your repository is public (General, Change repository visibility).
+
+If you haven't already, send your teacher the url of your github solutions repository via Teams chat.
+The url looks something like https://github.com/<your_user_name/Solutions.git
+
+Thereafter go on with the next file.
+"""
+
+print("Test successful")
